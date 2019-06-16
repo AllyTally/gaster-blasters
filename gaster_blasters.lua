7d625ada1b9023e0ad9cf0ab93771683207b73db
Invision <mariomario13245@gmail.com>
2019-06-16 01:28:05 -0300

    license

--- gaster_blasters.lua
+++ gaster_blasters.lua
@@ -1,6 +1,18 @@
--- https://github.com/Luigimaster1/gaster-blasters/
--- written by ally
--- please dont steal and claim as your own
+--[[
+    gaster_blasters.lua, a gaster blaster library for Create Your Frisk
+    https://github.com/Luigimaster1/gaster-blasters/
+    Copyright (C) 2019  Alexia Tilde
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]--
 
 local self = {}
 
